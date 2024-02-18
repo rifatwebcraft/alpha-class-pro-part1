b5ea0024c6033486c20626817ade85c603b18fb4
rifatwebcraft <cmohammadrifat@gmail.com>
2024-02-18 19:31:30 +0600

    almost finished the project

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -11,6 +11,10 @@
 function handleKeyboardButtonPress(event){
     const playerPressed = event.key ;
     console.log('player pressed', playerPressed);
+    // game over immedietly if i want
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
     // get the expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
@@ -20,16 +24,23 @@ function handleKeyboardButtonPress(event){
     // checked match or not
     if(playerPressed === expectedAlphabet){
         console.log('you got a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        const updateScore = currentScore + 1 ;
+        setTextElementValueById('current-score', updateScore)
+
+
+        // ------------------------------------------
     //    update score
     // 1.get the current score
-    const currentScoreElement = document.getElementById('current-score');
-    const currentScoreText = currentScoreElement.innerText;
-    const currentScore = parseInt(currentScoreText);
-    console.log(currentScore);
-    // 2,increase the score by 1
-    const newScore = currentScore + 1;
-    //3. show the update score
-    currentScoreElement.innerText = newScore;
+    // const currentScoreElement = document.getElementById('current-score');
+    // const currentScoreText = currentScoreElement.innerText;
+    // const currentScore = parseInt(currentScoreText);
+    // console.log(currentScore);
+    // // 2,increase the score by 1
+    // const newScore = currentScore + 1;
+    // //3. show the update score
+    // currentScoreElement.innerText = newScore;
 
         // next-round
         removeBackgroundColorId(expectedAlphabet);
@@ -37,14 +48,25 @@ function handleKeyboardButtonPress(event){
     }
     else{
         console.log('you missed you lost a life')
+
+        const currentLifeNumber = getTextElementValueById('current-life');
+        const newLife = currentLifeNumber - 1 ;
+        setTextElementValueById('current-life', newLife);
+
+        if(newLife === 0){
+            gameOver();
+        }
+
+
+        // ------------------------------------
         //1 get current life number
-        const currentLifeNumber = document.getElementById('current-life');
-        const currentLifeText = currentLifeNumber.innerText;
-        const currentLife = parseInt(currentLifeText);
-        // 2 reduce the life count
-        const newLife = currentLife - 1;
-        // show the update life number
-        currentLifeNumber.innerText = newLife;
+        // const currentLifeNumber = document.getElementById('current-life');
+        // const currentLifeText = currentLifeNumber.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        // // 2 reduce the life count
+        // const newLife = currentLife - 1;
+        // // show the update life number
+        // currentLifeNumber.innerText = newLife;
     }
 }
 // capture keyboard keypress
@@ -65,7 +87,26 @@ function continueGame (){
 
 
 function play(){
+    // only play playgroud hide other element
     hideElementById('home');
+    hideElementById('final-score')
     showElementById('play-ground');
+
+    // reseet score and life
+    setTextElementValueById('current-life', 5)
+    setTextElementValueById('current-score', 0)
     continueGame();
+}
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    // update final score
+    const lastscore = getTextElementValueById('current-score');
+    console.log(lastscore);
+    setTextElementValueById('game-score', lastscore)
+
+    // clear the last selected alphabet highlight
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorId(currentAlphabet);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -8,6 +8,24 @@ function showElementById(elementId){
     element.classList.remove('hidden');
 }
 
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
 function addBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400')
